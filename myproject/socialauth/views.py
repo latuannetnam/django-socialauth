15a31b66b784e5406b451008ef758c72aae58f9f
Le Anh Tuan <latuannetnam@gmail.com>
2015-11-15 18:10:21 +0700

    Pass origin's query string to logged page

--- myproject/socialauth/views.py
+++ myproject/socialauth/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render, render_to_response
 from django.contrib import auth
 from socialauth.models import UserProfile
 from django.http import HttpResponseRedirect
+from django.utils.encoding import *
+from  django.utils.http import *
 import logging
 logger = logging.getLogger(__name__)
 
 def login(request):
-    logger.debug("user: %s!" % request.user)
-    return render_to_response("socialauth/login.html",{"user":request.user})
+    queryString = request.META['QUERY_STRING']
+    queryStringEncode = urlquote(queryString)
+    logger.debug("request: %s!" % queryStringEncode)
+    return render_to_response("socialauth/login.html",{"user":request.user, "query_string":queryStringEncode})
 
 def logged(request):
     return render_to_response("socialauth/logged.html",{"user":request.user})
